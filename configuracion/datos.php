c2b053038618f97cdc57bb0734a2a1d62eb2fd45
Sebastian <sebastianmg@gmail.com>
2026-07-01 10:37:10 -0500

    Se ajusta vantana de configuracion de datos de la unidad

--- configuracion/datos.php
+++ configuracion/datos.php
@@ -5,12 +5,11 @@
     $stmtPais = $conexion->query("SELECT id_pais AS id, nombre FROM paises ORDER BY nombre");
     $paises = $stmtPais->fetchAll(PDO::FETCH_ASSOC);
 
-    $stmtDepartamentos = $conexion->query("SELECT id_departamento AS id, nombre FROM departamentos ORDER BY nombre");
+    $stmtDepartamentos = $conexion->query("SELECT codigo, id_departamento AS id, nombre FROM departamentos ORDER BY nombre");
     $departamentos = $stmtDepartamentos->fetchAll(PDO::FETCH_ASSOC);
 
-    $stmtCiudades = $conexion->query("SELECT id_ciudad AS id, nombre FROM ciudades ORDER BY nombre");
+    $stmtCiudades = $conexion->query("SELECT id_ciudad AS id, nombre , codigo_dane FROM ciudades ORDER BY nombre");
     $ciudades = $stmtCiudades->fetchAll(PDO::FETCH_ASSOC);
-
 
 
     ?>
@@ -60,97 +59,125 @@
             <p>Antes de comenzar a utilizar el sistema, es necesario que completes la configuración inicial de la copropiedad. Por favor, proporciona la información requerida en los campos a continuación.</p>
             <p>Configura los datos básicos de la copropiedad para activar el sistema.</p>
             <br>
+            <br>
+            <h3>Datos de la copropiedad</h3>
+  
             <div class="bloque filtros">
-                <div class="card">
-                    <H4>País:</H4>
-                    <select name="id_pais" required>
-                        <option value="">Seleccione el país</option>
+                <div class="form-group label">
+                    <span class="step active">Nombre del Conjunto Residncial</span>
+                    <input type="text" id="nombre_unidad" name="nombre_unidad" placeholder="Ej. Urbanización Las Palmas" required>
+                </div>
+                <div class="form-group label">
+                    <span for="id_unidad">Identificación de la Unidad - NIT:</span>
+                    <input type="text" id="nit_unidad" name="nit_unidad" placeholder="Ingrese el NIT de la unidad" required>
+                </div>
+                <div class="form-group label">
+                    <span class="step active">Representante Legal</span>
+                    <input type="text" id="representante_legal" name="representante_legal" placeholder="Nombre del representante legal" required>
+                </div>
+
+            </div>  
+
+            <h3>Ubicación de la copropiedad</h3>
+            <div class="bloque filtros">
+                <div class="form-group label">
+                    <span class="step active">País</span>
+                    <select name="id_pais" class="form-control">
+                        <option value="">Seleccione un país</option>
                         <?php foreach ($paises as $pais): ?>
                             <option value="<?= $pais['id'] ?>">
-                                <?= htmlspecialchars($pais['nombre']) ?></option>
+                                <?= htmlspecialchars($pais['nombre']) ?>
+                            </option>
                         <?php endforeach; ?>
                     </select>
+                    
                     <button type="button" class="btn-secondary" id="btnNuevoPais">
                         + Nuevo país
                     </button>
                 </div>
 
-                <div class="card">
-                    <h4>Departamento:</h4>
-                    <select name="departamento_id" required>
+                <div class="form-group label">
+                    <span class="step active">Departamento</span>
+                    <select name="id_departamento" class="form-control">
                         <option value="">Seleccione un departamento</option>
-                        <?php foreach ($departamentos as $d): ?>
-                            <option value="<?= $d['id'] ?>">
-                                <?= htmlspecialchars($d['nombre']) ?>
+                        <?php foreach ($departamentos as $departamento): ?>
+                            <option value="<?= $departamento['id'] ?>">
+                                <?= htmlspecialchars($departamento['codigo']) ?> - <?= htmlspecialchars($departamento['nombre']) ?>
                             </option>
                         <?php endforeach; ?>
                     </select>
+                    
                     <button type="button" class="btn-secondary" id="btnNuevoDepartamento">
                         + Nuevo departamento
                     </button>
                 </div>
 
-                <div class="card">
-                    <h4>Ciudad:</h4>
-                    <select name="ciudad_id" required>
+                <div class="form-group label">
+                    <span class="step active">Ciudad</span>
+                    <select name="id_ciudad" class="form-control">
                         <option value="">Seleccione una ciudad</option>
                         <?php foreach ($ciudades as $c): ?>
                             <option value="<?= $c['id'] ?>">
-                                <?= htmlspecialchars($c['nombre']) ?>
+                                <?= htmlspecialchars($c['codigo_dane']) ?> - <?= htmlspecialchars($c['nombre']) ?>
                             </option>
                         <?php endforeach; ?>
                     </select>
-                    <button type="button" class="btn-secondary" id="btnNuevoDepartamento">
-                        + Nueva Ciudad
+
+                    <button type="button" class="btn-secondary" id="btnNuevaCiudad">
+                        + Nueva ciudad
                     </button>
                 </div>
-            </div>
 
-            <div class="bloque filtros">
-                <div class="card">
-                    <span class="step active">Nombre del Conjunto Residncial</span>
-                    <input type="text" id="nombre_unidad" name="nombre_unidad" placeholder="Ej. Urbanización Las Palmas" required>
-                </div>
-                <div class="card">
-                    <span for="id_unidad">Identificación de la Unidad - NIT:</span>
-                    <input type="text" id="nit_unidad" name="nit_unidad" placeholder="Ingrese el NIT de la unidad" required>
-                </div>
-                <div class="card">
-                    <span class="step active">Representante Legal</span>
-                    <input type="text" id="representante_legal" name="representante_legal" placeholder="Nombre del representante legal" required>
-                </div>
-
-                <div class="card">
+                <div class="form-group label">
                     <span for="direccion">Dirección o Ubicación:</span>
                     <input type="text" id="direccion" name="direccion" placeholder="Ej. Vía Las Palmas Km 4" required>
                 </div>
-                <div class="card">
+                <div class="form-group label">
                     <span for="sector">Sector:</span>
-                    <input type="text" id="sector" name="sector" placeholder="Comuna - Barrio - Zona" required>
+                    <input type="text" id="sector" name="sector" placeholder="Comuna - Barrio - Zona" >
                 </div>
 
-                <div class="card">
-                    <span class="step">Torres/Bloques</span>
-                    <input type="text" id="torre_bloque" name="torre_bloque" placeholder="Ej. Torre 1, Torre 2, Bloque A" required>
+                <div class="form-group label">
+                    <span class="step">Torres - Bloques</span>
+                    <input type="text" id="torre_bloque" name="torre_bloque" placeholder="Ej. Torre 1, Torre 2, Bloque A" >
                 </div>
-                <div class="card">
+                <div class="form-group label">
                     <span class="step">Unidades de vivienda</span>
-                    <input type="text" id="unidades_vivienda" name="unidades_vivienda" placeholder="Cantidad de viviendas" required>
+                    <input type="text" id="unidades_vivienda" name="unidades_vivienda" placeholder="Cantidad de viviendas" >
                 </div>
 
             </div>
 
 
-
+            <h3>Datos de contacto</h3>
             <div class="bloque filtros">
                 <div class="card">
                     <span for="correo_propietario">Correo Electrónico:</span>
-                    <input type="email" id="correo_propietario" name="correo_propietario" placeholder="carlos.propietario@email.com" required>
+                    <input type="email" id="correo_propietario" name="correo_propietario" placeholder="carlos.propietario@email.com" >
                 </div>
 
                 <div class="card">
                     <span for="telefono_propietario">Teléfono de Contacto:</span>
-                    <input type="tel" id="telefono_propietario" name="telefono_propietario" placeholder="310 555 4321" required>
+                    <input type="tel" id="telefono_propietario" name="telefono_propietario" placeholder="Ingrese telefono de contacto" >
+                </div>
+
+            </div>
+            <h3>Documentos</h3>
+            <div class="bloque filtros">
+                <div class="form-card">
+                    <span for="reglamento">Reglamento de propiedad horizontal: (PDF)</span>
+                    <input type="file" id="reglamento" name="reglamento" placeholder="Cargue el reglamento" accept="image/*,application/pdf">
+                </div>
+
+                <div class="form-card">
+                    <span for="manual">Manual de convivencia: (PDF)</span>
+                    <input type="file" id="manual" name="manual" placeholder="Cargue el manual" accept="image/*,application/pdf">
+                </div>
+
+                <div class="logo-group">
+                    <span for="logo">Logo de la unidad</span>
+                    <input type="file" id="logo" name="logo" placeholder="Cargue el logo" accept="image/*">                    
+                        <img src="../assets/Img/user.png" alt="logo" id="logoPreview" class="preview-logo">
                 </div>
 
             </div>
@@ -169,11 +196,16 @@
                 <span id="cerrarPais" class="cerrar">&times;</span>
 
                 <h2>Nuevo País</h2>
-
+                <br><br>
                 <form action="../actions/guardar_pais.php" method="POST">
                     <label>Nombre del país</label>
+                    <br>
                     <input
                         type="text" id="nombrePais" name="nombreP" required maxlength="100">
+                    <br><br>
+                    <label>Codigo del país</label>
+                    <input
+                        type="text" id="codigoPais" name="codigoP" maxlength="10"> 
                     <br><br>
                     <button type="reset" class="btn-limpiar" id="cancelarPais">Cancelar</button>
                     <button type="submit" class="btn-filtrar">Guardar</button>
@@ -193,6 +225,10 @@
                     <input
                         type="text" id="nombreDepartamento" name="nombreD" required maxlength="100">
                     <br><br>
+                    <label>Código del departamento</label>
+                    <input
+                        type="text" id="codigoDepartamento" name="codigo" maxlength="10">
+                    <br><br>
                     <button type="reset" class="btn-limpiar" id="cancelarDepartamento">Cancelar</button>
                     <button type="submit" class="btn-filtrar">Guardar</button>
 
@@ -211,12 +247,14 @@
                     <input
                         type="text" id="nombreCiudad" name="nombreC" required maxlength="100">
                     <br><br>
+                    <label>Código de la ciudad</label>
+                    <input
+                        type="text" id="codigoCiudad" name="codigo" maxlength="10">
                     <button type="reset" class="btn-limpiar" id="cancelarCiudad">Cancelar</button>
                     <button type="submit" class="btn-filtrar">Guardar</button>
 
                 </form>
             </div>
-        </div>
 
     </body>
 
